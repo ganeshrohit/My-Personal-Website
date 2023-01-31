278abc245803130d2d3c5bbecc07b7e9ccfee895
ganeshrohit <bg.rohit7@gmail.com>
2023-01-31 00:27:22 -0500

    added the creation of a new post feature and editing the post feature

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name = 'post_list'),
     path('post/<int:pk>/', views.detailed_post, name = 'detailed_post'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
 
